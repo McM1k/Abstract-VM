397dcfa6a18167ba34e83ad440d5e655690c922a
McM1k <gboudrie@student.42.fr>
2018-02-13 13:58:52 +0100

    WIP - adding OperandFactory

--- sources/OperandFactory.hpp
+++ sources/OperandFactory.hpp
@@ -24,6 +24,8 @@ public:
 
 private:
 
+	std::array
+
 	IOperand const * createInt8( std::string const & value ) const;
 	IOperand const * createInt16( std::string const & value ) const;
 	IOperand const * createInt32( std::string const & value ) const;
